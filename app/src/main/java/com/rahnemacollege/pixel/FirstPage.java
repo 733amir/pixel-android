1fff0f8031a48a1193e37b706208faecb5860fcb
Amir Khazaie <733amir@gmail.com>
2017-11-09 15:21:00 +0330

    Add first-page to select a login or signup button. This will seperate work and code needed for login and signup page.

--- app/src/main/java/com/rahnemacollege/pixel/FirstPage.java
+++ app/src/main/java/com/rahnemacollege/pixel/FirstPage.java
@@ -1,7 +1,9 @@
 package com.rahnemacollege.pixel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstPage extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class FirstPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+    }
+
+    public void onLogin(View view) {
+        Intent toLogin = new Intent(this, LoginActivity.class);
+        startActivity(toLogin);
+    }
+
+    public void onSignup(View view) {
+        Intent toSignup = new Intent(this, SignupActivity.class);
+        startActivity(toSignup);
     }
 }
