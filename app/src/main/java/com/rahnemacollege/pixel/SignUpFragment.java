458d6a0ab18629dd901d18d3dd189feb38b80107
Amir Khazaie <733amir@gmail.com>
2017-11-12 23:56:23 +0330

    Renaming some file to the convention between Android Developer Team.

--- app/src/main/java/com/rahnemacollege/pixel/SignUpFragment.java
+++ app/src/main/java/com/rahnemacollege/pixel/SignUpFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +21,7 @@ public class SignUpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_signup, container, false);
+        return inflater.inflate(R.layout.fragment_signup, container, false);
     }
 
 }
