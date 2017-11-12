0dd6945f77d3e27141bc8c1c0efa86b8f356984f
Amir Khazaie <733amir@gmail.com>
2017-11-12 23:28:21 +0330

    Converted first page with two buttons to a with a tablayout and viewpager. Converted Login and Sign Up activity to fragment for using in view pager.

--- app/src/main/java/com/rahnemacollege/pixel/FirstPage.java
+++ app/src/main/java/com/rahnemacollege/pixel/FirstPage.java
@@ -1,16 +1,30 @@
 package com.rahnemacollege.pixel;
 
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.support.design.widget.BottomNavigationView;
 
-public class FirstPage extends AppCompatActivity {
+public class FirstPage extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+
+        ViewPager viewPager = findViewById(R.id.first_page_container);
+        FirstPageFragmentAdapter firstPageFragmentAdapter = new FirstPageFragmentAdapter(this, getSupportFragmentManager());
+        viewPager.setAdapter(firstPageFragmentAdapter);
+        TabLayout tabLayout = findViewById(R.id.first_page_tab);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     public void onLogin(View view) {
---
+++ app/src/main/java/com/rahnemacollege/pixel/FirstPageFragmentAdapter.java
@@ -0,0 +1,54 @@
+package com.rahnemacollege.pixel;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class FirstPageFragmentAdapter extends FragmentPagerAdapter {
+
+    private Context mContext;
+
+    public FirstPageFragmentAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    // This determines the fragment for each tab
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new LoginFragment();
+        } else {
+            return new SignUpFragment();
+        }
+    }
+
+    // This determines the number of tabs
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    // This determines the title for each tab
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.firstpage_b_login);
+            case 1:
+                return mContext.getString(R.string.firstpage_b_signup);
+            default:
+                return null;
+        }
+    }
+
+}
